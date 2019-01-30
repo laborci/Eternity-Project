4d9304736aeab534f165abd8c078c4ddbd981945
Gergely Laborci <gergely@laborci.hu>
2019-01-30 15:43:01 +0100

    git hack

---
+++ config/local/environment.php
@@ -0,0 +1,8 @@
+<?php 
+
+return [
+	'DOMAIN'   => 'YOUR.DEV.DOMAIN',
+	'DATABASE' => "mysql://root:root@".(getenv('CONTEXT') === 'CLI' ? '127.0.0.1' : 'mariadb' ).":3306/YOUR-DATABASE?charset=utf8",
+	'DEV_MODE' => true,
+	'ROBOTMONKEY_ID'=>13,
+];
