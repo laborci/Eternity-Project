cd4fb709e2a9d2ba9dbb97fa2aded59b2fd35bc3
Gergely Laborci <gergely@laborci.hu>
2019-03-11 15:36:56 +0100

    Update UserRepository.php

--- Application/Entity/User/UserRepository.php
+++ Application/Entity/User/UserRepository.php
@@ -10,5 +10,5 @@ class UserRepository extends \RedFox\Entity\Repository implements AuthenticableR
 	use Helpers\RepositoryTrait;
 
 	public function authLookup($id): AuthenticableInterface { return $this->search(Filter::where("status='active'")->and('id = $1', $id))->pick(); }
-	public function authLoginLookup($login): AuthenticableInterface { return $this->search(Filter::where("status='active'")->and('email = $1', $login))->pick(); }
+	public function authLoginLookup($login): ?AuthenticableInterface { return $this->search(Filter::where("status='active'")->and('email = $1', $login))->pick(); }
 }
