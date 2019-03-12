7fbfc5432949a97f0b773847d28a35d6ef57ac93
Laborci Gergely <gergely@laborci.hu>
2019-03-12 13:51:12 +0100

    Update entity-generator.php

--- etc/cfg/entity-generator.php
+++ etc/cfg/entity-generator.php
@@ -1,7 +1,7 @@
 <?php return (new \Eternity\ConfigBuilder\ConfigSegment())
 	->interface(\RedFox\EntityGenerator\EntityGeneratorConfigInterface::class)
 	->value([
-		'path'             => env('ROOT').'Entity/',
+		'path'             => env('ROOT').'Application/Entity/',
 		'databases'        => [
 			'database' => \AppPDOConnection::class,
 		],
